a788e51e374ac31946a9b05b1c117a0e0e8a5021
SANTHOSH KUMAR GOWDA <gowda.santhu1939@gmail.com>
2025-04-01 20:32:32 -0400

    Add files via upload

---
+++ fundamentals.py
@@ -0,0 +1,292 @@
+# num_1=1500
+# num_2=500
+# result=(num_1/num_2)
+# print(result)
+# # above the code used to express the divison of 2 numbers
+# '''
+# i have taken two numberical values 
+# performed division operation of that two numerical values,
+#  then display the result using the print
+#  '''
+
+# rollnumber = 91148
+# print(id(rollnumber))
+
+# _customer_name = 'santhu'
+# print(id(_customer_name))
+
+      
+# # _Hdfc_accountnumber = 12345
+# print(id(_Hdfc_accountnumber))
+
+# transcation = 123
+# Transcation = 1234
+# TRANSCATION = 12345
+# print(transcation)
+# print(Transcation)
+# print(TRANSCATION)
+
+# number_1 =278 # 1st numeric value
+# number_2 = 523 #2nd numeric value
+# number_3 =(number_1+number_2)# addition of 2 numericals values
+# print(number_3)# result excecuted number_3 by using print
+# number_4=(number_1*number_2)# multiplication of 2 numerical values
+# print(number_4)# result excecuted number_4 by using print
+# final_number =(number_3+number_4)# final number is addition of number 3 and number 4
+# print(final_number)#result excecuted final_number by using print
+
+# account_balance = 10000
+# print(type(account_balance))
+
+# percentage_intrest = 10.56
+# print(type(percentage_intrest))
+
+# customer_name = "santhoshkumar"
+# print(type(customer_name)
+
+############################# feb 06 #############################
+
+# CODING EXCERCISE
+
+
+# #1.print statment-write a progaramme print your name
+# name = "SANTHOSH"
+# print(name)
+
+#2.comments - create a python script with both single line and multi line comments explning purpose of script
+
+# Bank_name = "HDPC" # this line showing customer bank_name
+# print(Bank_name) #result excecutes bank_name by using print command
+# customer_name = "santhosh" # this line showing customer_name
+# print(customer_name)#result excecutes bank_name by using print command
+# account_balance = 10000 # this line showing customer account balance
+# print(account_balance)#result excecutes bank_name by using print command
+
+# '''
+# above the code telling customer bank account name and customer name,
+# How much he maintain balance in his account
+# '''
+
+#3data types
+#Define list containing 3 different type of datatypes
+# list_1 = [1,2,3,5.9,"sam"]
+# print(list_1)
+# print(type(list_1))
+
+# #4string operations
+# #concatenate two strings and print result
+# str_1 = "Hi"
+# str_2 = "welcome to india"
+# result = str_1+str_2
+# print(result)
+
+# 4-Repeat a string three times and display the output
+# bank_names="HDFC"
+# print(bank_names*3)
+
+#5- Python keywords
+#Create a variable with a name that is a python keyword.what happend.
+# None = "1"
+# print(None)
+
+# OUTPUT:    None = "1"
+# SyntaxError: cannot assign to None
+
+#6 Python variables
+# #Declare two variables,one storing as intrger and other one string.print their values
+# roll_number=1234
+# name= "Santhosh"
+# result =(roll_number,name)
+# print(result)
+
+
+#7 Type conversation
+# convert a float to integer and print the result
+
+# percentage= 78.56
+# print(type(percentage))
+# float_conversion= int(percentage)
+# print(float_conversion)
+# print(type(float_conversion))
+
+# #8.simple input and output
+# #take the user age as input and print massage using that input
+# num_1=input("enter age:")
+# print(num_1)
+
+
+
+#EXCRCISE:
+
+# 1. Print statment
+# write a programm that prints a pattern using multiple statments
+# name_1="geetha"
+# print(name_1)
+# print(name_1,name_1)
+# print(name_1,name_1,name_1)
+
+# output:
+# geetha
+# geetha geetha
+# geetha geetha geetha
+
+#2.comments
+#create a python script for a simple task and comments to each step
+
+# bank_name="abcd" #line assigns bank name
+# customer_name ="sam" #line assigns customer name
+# num_visits= 4 #line assigns customer number of visits
+# balance=5000 #line assigns customer balnce
+# result =(bank_name,customer_name,num_visits,balance) #result express bank name,customername,no.of visits and balance
+# print(result)
+
+#output :('abcd', 'sam', 4, 5000)
+
+
+#3. Data structures & Data types.
+#implement programm that uses a dictionary to store information about a book(title,author,publicationyear)
+
+# dict_1={"title":"my life","author":"santhosh","publication_year":"2015"}
+# print(dict_1)
+
+#4.string operations
+# write a python program that takes a string as input 35 and return float value
+
+# str_1= 35
+# print(str_1)
+# print(type(str_1))
+# float_conversion=float(str_1)
+# print(float_conversion)
+
+# output:35.0
+
+# #5.concatenate strings:
+# # write a programm to take twonames as input and print them together
+# name_1="sam"
+# name_2="ram"
+# result=(name_1+name_2)
+# print(result)
+
+# output:samram
+
+# 6. Type conversions:
+# create a programm that takes user input for their age,converts it to an integer,adds 5,and then prints the result.
+# age= input("enter age:")
+# int_conversion= int(age)
+# result= (int(age)+5)
+# print(result)
+
+# output:
+# enter age:25
+# 30
+
+#7 simple input&output:
+# #Build a caluclator program that takes two numbers as input and performs the arthematic operation
+
+# num_1=int(input("enter num_1:"))
+# num_2= int(input("enter num_2:"))
+# result=(num_1*num_2)
+# print(result)
+
+# output:
+# enter num_1:10
+# enter num_2:20
+# 200
+
+
+########## Practice data types,typeconversion,input function:
+
+# 1.int:
+# num_1=15
+# print(type(num_1))
+
+# 2.float:
+percentage=15.5
+# # print(type(percentage))
+
+# # 3.string:
+# school_name="vidyaparishath"
+# print(type(school_name))
+
+#4.bool
+# cyberfruad= True
+# print(type(cyberfruad))
+
+#5.list
+# number_list=[1,2,3,4]
+# print(type(number_list))
+
+# 6.tuple
+# marks_list=(78,89,53,41,92)
+# print(type(marks_list))
+
+#7.set
+# ages={56,55,54,85,24,36}
+# print(type(ages))
+
+#8.dictionary
+# dict_1={"book_name":"my _life","author":"sam","year":"2055"}
+# print(type(dict_1))
+
+
+
+# #############################################
+
+# 1)set--->tuple
+
+# set_1={1,2,3,4}
+# print(type(set_1))
+# tuple_conversion=tuple(set_1)
+# print(tuple)
+
+# output:
+# <class 'set'>
+# <class 'tuple'>
+
+# # # 2)set-->list
+# num_1 ={1,2,3,4}
+# print(type(num_1))
+# list_conversion=list(num_1)
+# print(type(num_1))
+
+# output:
+# <class 'set'>
+# <class 'set'> not converted to list
+
+# 3.list--->set
+# rollnumber=[1,2,3,4]
+# print(type(rollnumber))
+# set_conversion=set(rollnumber)
+# print(type(rollnumber))
+
+
+# output:
+# <class 'list'>
+# <class 'list'> not converted list to set
+
+#4.tuple---->dict
+# numbe_list=(1,2,3,4)
+# print(type(numbe_list))# dict_conversion=dict(numbe_list)
+# print(type(dict))
+
+# output:
+# <class 'tuple'>
+# Traceback (most recent call last):
+#   File "c:\SAM\FEB_2025\PYTHON LIFE\fundamentals.py", line 270, in <module>
+#     dict_conversion=dict(numbe_list)
+#  ^^^^^^^^^^^^^^^^
+# TypeError: cannot convert dictionary update sequence element #0 to a sequence
+# tuple to dictionary conversion not done
+
+#5.dictionary--->tuple
+# dict_1={"name":"sam","age":"35"}
+# print(type(dict_1))
+# tuple_conversion=tuple(dict_1)
+# print(type(dict_1))
+
+# output:
+# <class 'dict'>
+# <class 'dict'>
+
+
+
